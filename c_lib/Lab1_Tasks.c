e3d3b4fde745c47f51db64cd44120b9ccb9c9e84
Lexi Hanlon <lexi.j.hanlon@gmail.com>
2026-02-05 14:10:27 -0700

    finished lab1_tasks file

--- c_lib/Lab1_Tasks.c
+++ c_lib/Lab1_Tasks.c
@@ -11,5 +11,16 @@ void Multiply_And_Send( float value_left, float value_right )
 }
 
 void Divide_And_Send(float value_left, float value_right){
-    
+    float ret_val = value_left / value_right;
+    USB_Send_Msg("cf", '/', &ret_val, sizeof(ret_val));
+}
+
+void Add_And_Send(float value_left, float value_right){
+    float ret_val = value_left + value_right;
+    USB_Send_Msg("cf", '+', &ret_val, sizeof(ret_val));
+}
+
+void Subtract_And_Send(float value_left, float value_right){
+    float ret_val = value_left - value_right;
+    USB_Send_Msg("cf", '-', &ret_val, sizeof(ret_val));
 }
