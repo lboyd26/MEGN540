ec88a704450e1a96f3d2e34a2c893ba2e614613e
Quinton Lorenz <quinton.lorenz33@gmail.com>
2026-02-22 15:48:41 -0700

    The big T should be working maybe

--- Lab2-Timing/Lab2-Timers.c
+++ Lab2-Timing/Lab2-Timers.c
@@ -66,6 +66,9 @@ void Initialize_Modules( float _time_not_used_ )
     // Setup message handling to get processed at some desired rate.
     Initialize_Task( &task_message_handling, Task_Message_Handling );
 
+    // Initializes lab 2 tasks
+    init();
+
     // Initialize_Task( &task_message_handling_watchdog, /*watchdog timout period*/,  Task_Message_Handling_Watchdog );
 }
 
@@ -84,6 +87,7 @@ int main( void )
 
 
         Task_Run_If_Ready( &task_message_handling );
+        Task_Run_If_Ready(&task_send_time);
         Task_Run_If_Ready( &task_restart );
 
         // Use this watchdog as the 100 ms no answer-buffer flush thing from the lab PDF
