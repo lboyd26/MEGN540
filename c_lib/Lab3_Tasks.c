c787e5a0ce3cd4a64d06066496a061fa29c9d5a2
NorlenL <linorlen3@gmail.com>
2026-03-11 23:04:55 -0600

    Update Lab3_Tasks.c

--- c_lib/Lab3_Tasks.c
+++ c_lib/Lab3_Tasks.c
@@ -127,3 +127,68 @@ void Battery_Filter_Update( float _time_since_last )
     Filter_Value( &battery_filter, Battery_Voltage() );
 }
 */
+
+
+
+
+#include "Lab3_Tasks.h"
+
+static Filter_Data_t battery_filter;
+static bool battery_filter_initalized = false;
+
+void Send_Loop_Battery( float _time_since_last )
+{
+    float voltage = Filter_Last_Output(&battery_filter);
+    USB_Send_Msg("cf", 'B', &voltage, sizeof(voltage));
+}
+
+void Send_Battery_Now( float _time_since_last )
+{
+    float voltage = Filter_Last_Output(&battery_filter);
+    USB_Send_Msg("cf", 'b', &voltage, sizeof(voltage));
+}
+
+void Send_Encoder_Now( float _time_since_last )
+{
+    struct __attribute__((__packed__)) { int32_t left; int32_t right; } data;
+    data.left  = Encoder_Counts_Left();
+    data.right = Encoder_Counts_Right();
+    USB_Send_Msg("cii", 'e', &data, sizeof(data));
+}
+
+void Send_Loop_Encoder( float _time_since_last )
+{
+    struct __attribute__((__packed__)) { int32_t left; int32_t right; } data;
+    data.left  = Encoder_Counts_Left();
+    data.right = Encoder_Counts_Right();
+    USB_Send_Msg("cii", 'E', &data, sizeof(data));
+}
+
+void Check_Battery_Voltage( float _time_since_last )
+{
+    if( !battery_filter_initalized ) return;
+    float bat = Filter_Last_Output(&battery_filter);
+    if( bat < 4.0f ) {
+        struct __attribute__((__packed__)) {
+            char let[7];
+            float volt;
+        } msg = { .let = {'B','A','T',' ','L','O','W'}, .volt = bat };
+        USB_Send_Msg("c7sf", '!', &msg, sizeof(msg));
+    }
+}
+
+void Battery_Filter_Update( float _time_since_last )
+{
+    if( !battery_filter_initalized ) {
+        float pi = 3.14159265358979323846f;
+        float T  = 0.002f;
+        float fc = 10.0f;
+        float a  = 2.0f * pi * fc * T;
+        float b_coeffs[] = { a / (1.0f + a), 0.0f };
+        float a_coeffs[] = { 1.0f, -1.0f / (1.0f + a) };
+        Filter_Init( &battery_filter, b_coeffs, a_coeffs, 1 );
+        Filter_SetTo( &battery_filter, Battery_Voltage() );
+        battery_filter_initalized = true;
+    }
+    Filter_Value( &battery_filter, Battery_Voltage() );
+}
